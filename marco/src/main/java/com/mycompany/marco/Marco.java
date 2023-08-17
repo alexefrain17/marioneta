d13f42471ac4d2adf671072b666c29df703d86f7
alexefrain17 <1432461@senati.pe>
2023-08-17 11:35:04 -0500

    agregue multiplicacion

--- marco/src/main/java/com/mycompany/marco/Marco.java
+++ marco/src/main/java/com/mycompany/marco/Marco.java
@@ -25,9 +25,13 @@ public class Marco {
         
         int resta = numero1+numero2;
         
+        int multipliucacion = numero1+numero2;
+        
         System.out.println("la suma es: "+ suma);
         
         System.out.println("la resta es: "+ suma);
+        
+        System.out.println("la multiplicacion  es: "+ suma);
         
     }
 }
