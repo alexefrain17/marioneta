f7eb4c13c121c09376f6dba84ebe9c808e59c8b7
alexefrain17 <1432461@senati.pe>
2023-08-17 11:19:24 -0500

    calculadora

---
+++ marco/src/main/java/com/mycompany/marco/Marco.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.marco;
+
+/**
+ *
+ * @author Alumno
+ */
+public class Marco {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
